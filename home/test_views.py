7941c00438f1b0b7732ac98724dd3d065b709d2d
manni8436 <emanuelsilva019@gmail.com>
2022-01-14 20:58:27 +0000

    Create home page views test

---
+++ home/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.shortcuts import reverse
+
+# Create your tests here.
+class TestHomeViews(TestCase):
+
+    def test_home_page_url_exists(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_the_home_url_is_accessible_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
