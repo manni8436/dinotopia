93b720646393409931a015498ad303576037b31d
manni8436 <emanuelsilva019@gmail.com>
2022-01-16 15:51:50 +0000

    Create sort name functionality

--- products/test_views.py
+++ products/test_views.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+
 from django.shortcuts import reverse
+from django.contrib.messages import get_messages
 from .models import Category, Product
 
 # Create your tests here.
@@ -22,6 +24,23 @@ class TestProductsViews(TestCase):
         response = self.client.get(reverse('products'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/products.html')
+
+    def test_sort_name_function(self):
+        category_name = 'adult_dinosaurs'
+        sort_array = ['name', 'category']
+        for sort in sort_array:
+            direction = 'asc'
+            current_sorting = f'{sort}_{direction}'
+            response = self.client.get(
+                f'/products/?category={category_name}'
+                f'&sort={sort}&direction={direction}')
+        self.assertEqual(current_sorting, f'{sort}_asc')
+        self.assertEqual(response.status_code, 200)
+
+    # def test_products_search_function(self):
+    #     response = self.client.get('/product/?', {'q': 'egg'})
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(response.context['search'], 'egg')
 
     def test_product_detail_page_url_exists(self):
         response = self.client.get('/products/1/')
