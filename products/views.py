0b58dff27661bcbfd63e68219a142360b801bcd6
manni8436 <emanuelsilva019@gmail.com>
2022-01-20 09:19:46 +0000

    Attempt at fixing bug on deployed site

--- products/views.py
+++ products/views.py
@@ -36,13 +36,12 @@ def all_products(request):
             categories = Category.objects.filter(name__in=categories)
 
 
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't search for any Dinosaurs")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
