f8df0345c375999ff645116052af6dac7becd58e
manni8436 <emanuelsilva019@gmail.com>
2022-01-23 14:43:49 +0000

    Add crispy forms to installed apps section in settings.py

--- dinotopia/settings.py
+++ dinotopia/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
     # Other
     'storages',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'dinotopia.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,8 +80,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
