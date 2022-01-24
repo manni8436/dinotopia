602df78e51d986e9b627005409bc32c285fb1319
manni8436 <emanuelsilva019@gmail.com>
2022-01-24 19:43:46 +0000

    Change max digit and decimal places values to rectify number field overflow error

--- checkout/models.py
+++ checkout/models.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 from products.models import Product
 
+
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
@@ -58,7 +59,7 @@ class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=10, decimal_places=4, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
