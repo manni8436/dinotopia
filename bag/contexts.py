a4fe9c56ba3c7502a1f93c0be3de39cdbb7f3415
manni8436 <emanuelsilva019@gmail.com>
2022-02-13 20:09:15 +0000

    Add docstring to bag_contents

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,8 +3,9 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-def bag_contents(request):
 
+def bag_contents(request):
+    """ function to add project to bag and show costs """
     bag_items = []
     total = 0
     product_count = 0
