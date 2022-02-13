1c3f028eebe0b1ec0b51fc6123e1976624ae9cff
manni8436 <emanuelsilva019@gmail.com>
2022-02-13 21:36:52 +0000

    Made test_models.py pep8 compliant

--- products/test_models.py
+++ products/test_models.py
@@ -6,15 +6,15 @@ class TestCategoryModels(TestCase):
 
     def test_category_str(self):
         """testing category models __str__"""
-        test_case = Category() # declared test_case as a Category class.
-        test_case.name = "testing" # declared test_case.name and changing the property instance (which has been declared as Category in the line above) as testing (which is a string)
-        self.assertEqual(str(test_case), "testing") # calling the __str__ function from inside the class.
+        test_case = Category()
+        test_case.name = "testing"
+        self.assertEqual(str(test_case), "testing")
 
     def test_category_friendly_name(self):
         """testing category models friendly name"""
         test_case = Category()
         test_case.friendly_name = "testing"
-        self.assertEqual(test_case.get_friendly_name(), "testing") # this is a more normal way of calling a class function.
+        self.assertEqual(test_case.get_friendly_name(), "testing")
 
 
 class TestProductModels(TestCase):
