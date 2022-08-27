bcebeacd16b43b9d9f32f18eef70af677809bbca
manni8436 <emanuelsilva019@gmail.com>
2022-08-27 16:01:04 +0000

    Render setup

--- dinotopia/settings.py
+++ dinotopia/settings.py
@@ -28,6 +28,10 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['dinotopia.herokuapp.com', 'localhost']
 
+# Add render URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
